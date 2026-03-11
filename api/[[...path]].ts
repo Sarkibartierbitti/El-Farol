e7844a46ad39ca65595b1c82ca4b3607c888c918
Mark Iakovlev <madmiakovlev@edu.hse.ru>
2026-03-11 11:54:28 +0300

    a

--- api/[[...path]].ts
+++ api/[[...path]].ts
@@ -1,4 +1,5 @@
 import { Hono } from 'hono';
+// @ts-expect-error - backend compiles to CJS, no .d.ts emitted
 import { app as backendApp } from '../apps/backend/lib/server.js';
 
 const app = new Hono().route('/api', backendApp);
